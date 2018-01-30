c67dc22c78cfdcf9b097508afbc372974137dad6
Joy Mun <jjlove410@naver.com>
2018-01-30 12:28:33 +0000

    from c9

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,24 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
-class SearchBar extends Component
+class SearchBar extends Component{
+  //define the state
+
 constructor(props){
   super(props);
 
   this.state = {term:""};
 }
   render(){
-    return <input onChange = {event => console.log(event.target.value)}/>;
+    return (
+    <div>
+  
+    <input 
+     value = {this.state.term}
+    onChange = {event => this.setState({term:event.target.value})} />
+     
+    </div>
+    );
   }
 
 }
